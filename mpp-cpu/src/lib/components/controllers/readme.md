69137394197ea4327d5e4dc88340d214ba6f62a3
Jon Lara <jon.laratrigo@gmail.com>
2024-05-17 19:19:01 +0200

    remove: mpp-core

--- mpp-cpu/src/lib/components/controllers/readme.md
+++
@@ -1,2 +0,0 @@
-Controller: two or more components that share same input
-Controllers functions are prefixed by `cll_`
--- mpp-cpu/src/linker/bus.h
+++
@@ -1,5 +0,0 @@
-#ifndef linker_bus_h
-#define linker_bus_h
-void init_linker_bus(void);
-void shutdown_linker_bus(void);
-#endif
