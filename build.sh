c54c111bfaf4e3917e36e9045a64d3bdc7cc31dc
Jon Lara <jon.laratrigo@gmail.com>
2024-05-18 18:43:08 +0200

    feat: improved build

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 set -e # exit on error
 
 _PREFIX=mpp-pynq
-CORE_CONTAINER_NAME="${_PREFIX}-core"
 FRONT_CONTAINER_NAME="${_PREFIX}-front"
-TARGET_PLATFORMS="linux/amd64,linux/arm/v7,linux/arm64/v8,linux/386,linux/ppc64le,linux/s390x"
-REPO="nullx0/mpp-pynq-front"
-VERSION=$(<VERSION)
 
+TARGET_PLATFORMS="linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x"
+REPO="nullx0/mpp-pynq-front"
+
+VERSION=$(<VERSION)
+API_URL=$1
+
+echo API_URL = $API_URL
 echo "This will build and push the following image to docker hub: $REPO:$VERSION"
 
 
 # build the server for all platforms
-docker buildx build --platform $TARGET_PLATFORMS --push -t $REPO:$VERSION -t $REPO:latest $FRONT_CONTAINER_NAME -f $FRONT_CONTAINER_NAME/Dockerfile
+docker buildx build --platform $TARGET_PLATFORMS --push -t $REPO:$VERSION -t $REPO:latest --build-arg REACT_APP_API_URL=$API_URL $FRONT_CONTAINER_NAME
